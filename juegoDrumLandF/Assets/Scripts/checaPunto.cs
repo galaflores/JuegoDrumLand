a82962a482e102566ae4aff1d6771cbf16265209
YunoeSd <99697763+YunoeSd@users.noreply.github.com>
2022-04-30 14:40:50 -0500

    este

--- juegoDrumLandF/Assets/Scripts/checaPunto.cs
+++ juegoDrumLandF/Assets/Scripts/checaPunto.cs
@@ -17,7 +17,6 @@ public class checaPunto : MonoBehaviour
     {
         toca = true;
 
-
     }
 
     // Update is called once per frame
@@ -33,23 +32,21 @@ public class checaPunto : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.L) && toca)
         {
-            toca = false;
             // Hace visible la explosi�n
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
             // oculta la nota
             GetComponent<SpriteRenderer>().enabled = false;
-            Destroy(gameObject, 0.3f);
+            Destroy(gameObject);
 
-            SaludPersonaje.instance.puntos++;
+            SaludPersonaje.instance.puntos =+1;
             HUD.instance.ActualizarPuntos();
-
+            toca = false;
         }
         else if (Input.GetKeyDown(KeyCode.L) && !toca)
         {
             //Descontar puntos
-            SaludPersonaje.instance.puntos--;
-            HUD.instance.ActualizarPuntos();
-            if (SaludPersonaje.instance.puntos <= 0 && SaludPersonaje.instance.vidas == 0)
+            SaludPersonaje.instance.vidas--;
+            if (SaludPersonaje.instance.vidas == 0)
             {
                 Destroy(Player, 0.1f);
             }
