611897798f6ff0543552258a5cefe1b57bf321c6
oviedo-13 <95649475+oviedo-13@users.noreply.github.com>
2022-05-03 22:32:06 -0500

    tonyYaCasi

--- juegoDrumLandF/Assets/Scripts/PeleaJefe.cs
+++ juegoDrumLandF/Assets/Scripts/PeleaJefe.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PeleaJefe : MonoBehaviour
 {
     public static PeleaJefe instance;
 
     public int notasSeguidas = 0;
+
+    private int vidasJefe = 3;
+    private int disparos = 5;
+
+    [SerializeField]
+    private Image CorazonJ1;
+    [SerializeField]
+    private Image CorazonJ2;
+    [SerializeField]
+    private Image CorazonJ3;
 
 
     private void Awake()
@@ -36,7 +48,28 @@ public class PeleaJefe : MonoBehaviour
                 }
             }
 
+
+
+
+
+
         }
         
+    }
+
+    private void ActualizarVidasJefe()
+    {
+        switch (vidasJefe)
+        {
+            case 2:
+                CorazonJ3.enabled = false;
+                break;
+            case 1:
+                CorazonJ2.enabled = false;
+                break;
+            case 0:
+                CorazonJ1.enabled = false;
+                break;
+        }
     }
 }
